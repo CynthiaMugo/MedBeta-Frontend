6c18dcdaa8f277a6efaed965d119d47517c7192a
Cynthia Mugo <cynthiamugo25@gmail.com>
2025-10-16 20:58:56 +0300

    Configure tailwind css

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import { useState } from 'react'
-
+import "./index.css"
 
 function App() {
 
   return (
     <>
-      <div>
-        <p>Welcome to MedBeta!</p>
-      </div>
+      <div className="min-h-screen flex items-center justify-center bg-gradient-to-r from-teal-100 to-blue-200">
+      <p className="text-4xl font-bold text-teal-700 bg-white px-6 py-4 rounded-xl shadow-md">
+        Welcome to MedBeta!
+      </p>
+    </div>
       
     </>
   )
